dc8af86514d5f7cfa305cc03c644c88c6305ef58
Tejas Soni <tejas.tejas.soni3@gmail.com>
2026-07-25 20:46:01 +0530

    .env example file uploads

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,6 +1,8 @@
 package config
 
 const (
-	APPNAME    = "User REST API"
-	APPVERSION = "v1.0"
+	APP_NAME    = getEnv("APP_NAME", "User REST API")
+	APP_URL     = getEnv("APP_URL", "http://localhost")
+	APP_PORT    = getEnv("APP_PORT", "8080")
+	APP_VERSION = "v1.0"
 )
